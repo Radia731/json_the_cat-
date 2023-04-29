fb02451b1535532dd1ff18c0ff373c3557f9ed07
Radia Ahmed <rad@Radias-Air.ht.home>
2023-04-29 04:27:56 -0400

    refactoring

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,20 @@
 const request = require('request');
 
-const breedName = process.argv[2];
+const fetchBreedDescription = function (breedName, callback) {
+    request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+        if (error) {
+            return callback(error, null);
+        } else if (response.statusCode === 404) {
+            return callback('Breed not found', null);
+        } else {
+            const data = JSON.parse(body);
+            if (data.length === 0) {
+                return callback('Breed not found', null);
+            } else {
+                return callback(null, data[0].description.trim());
+            }
+        }
+    });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-    if (error) {
-        console.log(`Failed to connect to API: ${error}`);
-    } else if (response.statusCode === 404) {
-        console.log('Breed not found');
-    } else {
-        const data = JSON.parse(body);
-        console.log(data[0].description);
-        console.log(typeof data);
-    }
-});
+module.exports = { fetchBreedDescription };
